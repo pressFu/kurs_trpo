39c41f6af2e659889d20f9579e6b7230c778bf43
Gorodenko Vlad <gorodenko.00@mail.ru>
2020-09-28 15:22:04 +0300

    Add function sorted words

--- kurs_trpo.cpp
+++ kurs_trpo.cpp
@@ -20,7 +20,14 @@ int main() {
 			fl = 0;
 		}
 	}
-
+	for (int j = num - 1; j > 0; j--)
+		for (int i = 0; i < j; i++)
+			if (strcmp(&str[words[i]], &str[words[i + 1]]) > 0)
+			{
+				int temp = words[i];
+				words[i] = words[i + 1];
+				words[i + 1] = temp;
+			}
 
 
 	return 0;
