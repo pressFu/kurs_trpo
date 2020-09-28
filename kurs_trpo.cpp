836ce8776e336fd3de2bc3f45a021d02c3057a98
Gorodenko Vlad <gorodenko.00@mail.ru>
2020-09-28 15:19:05 +0300

    Add sort words

--- kurs_trpo.cpp
+++ kurs_trpo.cpp
@@ -1,10 +1,25 @@
-﻿#include <stdio.h>
-#include <string.h>
+﻿#include <string.h>
 #include <iostream>
-
+#include <conio.h>
+using namespace std;
 
 int main() {
-
+	setlocale(LC_ALL, "Rus");
+	char str[5000];
+	char words[1500];
+	int num = 0;
+	gets_s(str);
+	int fl;
+	for (int i = 0, fl = 1; str[i]; i++) {
+		if (str[i] == ' ') {
+			str[i] = 0;
+			fl = 1;
+		}
+		else if (fl) {
+			words[num++] = i;
+			fl = 0;
+		}
+	}
 
 
 
