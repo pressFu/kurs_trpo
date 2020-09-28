3b247ea465f637a7a483bc1a5fd6d6764b0ee86a
Gorodenko Vlad <gorodenko.00@mail.ru>
2020-09-28 15:44:55 +0300

    fix bag

--- kurs_trpo.cpp
+++ kurs_trpo.cpp
@@ -3,13 +3,19 @@
 #include <conio.h>
 using namespace std;
 
+void print_words(int num, char* words, char* str) {
+	for (int i = 0; i < num; i++) {
+		printf("%s\n", &str[words[i]]);
+	}
+}
+
 int main() {
 	setlocale(LC_ALL, "Rus");
 	char str[5000];
 	char words[1500];
 	int num = 0;
+	cout << "Введите текст: " << endl;
 	gets_s(str);
-	int fl;
 	for (int i = 0, fl = 1; str[i]; i++) {
 		if (str[i] == ' ') {
 			str[i] = 0;
@@ -20,17 +26,17 @@ int main() {
 			fl = 0;
 		}
 	}
-	for (int j = num - 1; j > 0; j--)
-		for (int i = 0; i < j; i++)
+
+	for (int j = num - 1; j > 0; j--) {
+		for (int i = 0; i < j; i++) {
 			if (strcmp(&str[words[i]], &str[words[i + 1]]) > 0)
 			{
 				int temp = words[i];
 				words[i] = words[i + 1];
 				words[i + 1] = temp;
 			}
-
-	for (int i = 0; i < num; i++) {
-		printf("%s\n", &str[words[i]]);
+		}
 	}
+	print_words(num, words, str);
 	return 0;
 }
